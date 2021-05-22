89ac0460b058cf602790f3c77075243e10d23248
sluongo718 <sluongo718@yahoo.com>
2021-05-22 14:04:15 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(name: params[:name])
+        return head(:forbidden) unless user.authenticate(params[:password])
+        session[:user_id] = user_id
+        redirect_to users_path(user)
+    end
+end
